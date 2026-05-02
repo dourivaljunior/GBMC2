e09f524855044220c98e8b934e9fba85d85fb322
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2026-05-02 11:54:37 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ export const CONFIG = {
   // ID da pasta raiz no Google Drive
   ROOT_FOLDER_ID: "1wE8PTP74rEIkOPIXVqHxy2bCuuRQRaa5",
   // URL do Web App publicado a partir do Apps Script (Code.gs)
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbywSHZf36zGAywDEnCq6Y7tkt7aqUNdnP0ltHBAOH4sjtDFRHjQzoj0RhbsK4GJVJhNzw/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzOYqee1T7UqwVnI4UQwU5moEF_pyuxJTMI1DpFr7ZzucTGpLqpC5q2B5iJkvwdAQss/exec",
 };
